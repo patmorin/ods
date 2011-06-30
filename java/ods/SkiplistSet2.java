0640926f64d00cd7d4f1339cbee877156a253167
Pat Morin <morin@scs.carleton.ca>
2011-06-30 08:49:01 -0400

    SkiplistSet with a slicker add(x) method

---
+++ java/ods/SkiplistSet2.java
@@ -0,0 +1,35 @@
+package ods;
+
+import java.lang.reflect.Array;
+
+public class SkiplistSet2<T> extends SkiplistSet<T> {
+	Node[] stack;
+	
+	public SkiplistSet2() {
+		super();
+		stack = (Node[])Array.newInstance(Node.class, sentinel.next.length);
+	}
+	
+	public boolean add(T x) {
+		Node u = sentinel;
+		int r = h;
+		int comp = 0;
+		while (r >= 0) {
+			while (u.next[r] != null && (comp = c.compare(u.next[r].x,x)) < 0)
+				u = u.next[r];
+			if (u.next[r] != null && comp == 0) return false;
+			stack[r--] = u;
+		}
+		Node w = new Node(x, pickHeight());
+		if (w.height() > h)
+			while (h < w.height())
+				stack[++h] = sentinel;
+		for (int i = 0; i < w.next.length; i++) {
+			w.next[i] = stack[i].next[i];
+			stack[i].next[i] = w;
+		}
+		n++;
+		return true;
+	}
+
+}
