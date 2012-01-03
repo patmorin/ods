c73ac42b96b45e2dde485564661936a30994872b
Pat Morin <morin@scs.carleton.ca>
2012-01-03 13:34:49 -0500

    Forgotten files

---
+++ cpp/GeomVector.h
@@ -0,0 +1,28 @@
+
+#include "utils.h"
+#include "array.h"
+
+namespace ods {
+
+class GeomVector {
+protected:
+	array<int> x;
+
+public:	
+	unsigned hashCode() {
+		long p = (1L<<32)-5;   // prime: 2^32 - 5
+		long z = 0x64b6055aL;  // 32 bits from random.org
+		int z2 = 0x5067d19d;   // random odd 32 bit number
+		long s = 0;
+		long zi = 1;
+		for (int i = 0; i < x.length; i++) {
+			long long xi = (ods::hashCode(x[i]) * z2) >> 1; // reduce to 31 bits
+			s = (s + zi * xi) % p;
+			zi = (zi * z) % p;	
+		}
+		s = (s + zi * (p-1)) % p;
+		return (int)s;
+	}
+};
+
+} // namespace ods
---
+++ cpp/Point3D.h
@@ -0,0 +1,22 @@
+
+#include "utils.h"
+
+namespace ods {
+
+class Point3D {
+protected:
+	int x0, x1, x2;
+
+public:	
+	unsigned hashCode() {
+		long long z[] = {0x2058cc50L, 0xcb19137eL, 0x2cb6b6fdL}; // random
+		long zz = 0xbea0107e5067d19dL;                      // random
+		long h0 = ods::hashCode(x0);
+		long h1 = ods::hashCode(x1);
+		long h2 = ods::hashCode(x2);
+		return (int)(((z[0]*h0 + z[1]*h1 + z[2]*h2)*zz) >> 32);
+	}
+};
+
+} // namespace ods
+
