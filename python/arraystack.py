18510222cab2e674a2f38b3007de6f855fa0e35b
Pat Morin <morin@scs.carleton.ca>
2013-09-20 13:03:52 -0400

    Small touchups

--- python/arraystack.py
+++ python/arraystack.py
@@ -4,6 +4,8 @@ Created on 2012-04-02
 @author: morin
 '''
 
+def drange(a, b, c):
+    return range(a, b, c)
         
 class ArrayStack(object):
     def __init__(self):
@@ -11,35 +13,35 @@ class ArrayStack(object):
         self.n = 0
 
     def get(self, i):
-        if (i < 0 or i >= self.n): raise IndexError()
+        if i < 0 or i >= self.n: raise IndexError()
         return self.a[i]
 
-    def set(self, i, x): #@ReservedAssignment
-        if (i < 0 or i >= self.n): raise IndexError()
+    def set(self, i, x):
+        if i < 0 or i >= self.n: raise IndexError()
         y = self.a[i]
         self.a[i] = x
         return y
 
     def add(self, i, x): 
-        if (i < 0 or i > self.n): raise IndexError()
-        if (self.n == len(self.a)): self._resize()
-        for j in xrange(self.n, i, -1):
+        if i < 0 or i > self.n: raise IndexError()
+        if self.n == len(self.a): self._resize()
+        for j in drange(self.n, i, -1):
             self.a[j] = self.a[j-1]
         self.a[i] = x
         self.n += 1
 
     def remove(self, i): 
-        if (i < 0 or i >= self.n): raise IndexError()
+        if i < 0 or i >= self.n: raise IndexError()
         x = self.a[i];
-        for j in xrange(i, self.n-1): 
+        for j in range(i, self.n-1): 
             self.a[j] = self.a[j+1];
         self.n -= 1
-        if (len(self.a) >= 3*self.n): self._resize();
+        if len(self.a) >= 3*self.n: self._resize();
         return x;
   
     def _resize(self):
         b = [None] * max(1, 2*self.n)
-        for i in xrange(self.n):
+        for i in range(self.n):
             b[i] = self.a[i]
         self.a = b
         
