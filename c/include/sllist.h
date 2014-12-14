4e467fff9cde7bb2cc4d1d8820198df41fb3201f
tekinozbek <tekin@tekinozbek.com>
2014-12-13 20:38:01 -0800

    SLList documentation

--- c/include/sllist.h
+++ c/include/sllist.h
@@ -27,17 +27,71 @@ typedef struct {
 
 } sllist_t;
 
+/* FUNCTION
+ *      sllist_dispose
+ *
+ * DESCRIPTION
+ *      Releases all memory allocated by other sllist_* functions. If you have
+ *      any pointer references to nodes, they will be invalid after a call to
+ *      this function.
+ *
+ * PARAMETERS
+ *      l           A valid pointer to an initialized sllist_t struct.
+ */
 extern void sllist_dispose(sllist_t* l);
 
+/* FUNCTION
+ *      sllist_enqueue
+ *
+ * DESCRIPTION
+ *      Adds an element to the tail of the list.
+ *
+ * PARAMETERS
+ *      l           A valid pointer to an initialized sllist_t struct.
+ *      elem        Pointer to the element that will be inserted. Cannot be
+ *                  null.
+ */
 extern void sllist_enqueue(sllist_t* l,
                            void* elem);
 
+/* FUNCTION
+ *      sllist_init
+ *
+ * DESCRIPTION
+ *      Initializes an sllist_t struct.
+ *
+ * PARAMETERS
+ *      l           A valid pointer to an initialized sllist_t struct.
+ *      elem_size   Size of the elements that will be stored in the list.
+ */
 extern void sllist_init(sllist_t* l,
                         size_t elem_size);
 
+/* FUNCTION
+ *      sllist_pop (sllist_dequeue)
+ *
+ * DESCRIPTION
+ *      Pops an element from the head of the list.
+ *
+ * PARAMETERS
+ *      l           A valid pointer to an initialized sllist_t struct.
+ *      elem_out    If not null, the popped element will be copied into this
+ *                  memory.
+ */
 extern void sllist_pop(sllist_t* l,
                        void* elem_out);
 
+/* FUNCTION
+ *      sllist_push
+ *
+ * DESCRIPTION
+ *      Pushes an element to the head of the list.
+ *
+ * PARAMETERS
+ *      l           A valid pointer to an initialized sllist_t struct.
+ *      elem        Pointer to the element that will be inserted. Cannot be
+ *                  null.
+ */
 extern void sllist_push(sllist_t* l,
                         void* elem);
 
