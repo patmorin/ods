15e380a799336df87de775fa917346d9f416de0f
Tekin Ozbek <tekin@tekinozbek.net>
2015-08-23 16:37:47 -0400

    Binary search tree complete

--- c/src/binarysearchtree.c
+++ c/src/binarysearchtree.c
@@ -51,36 +51,89 @@ static btnode_t* make_node(binarysearchtree_t* tree, btnode_t* p, btnode_t* l,
     return new_node;
 }
 
-void binarysearchtree_add(binarysearchtree_t* tree, void* data) {
+static void remove_node(binarysearchtree_t* tree, btnode_t* node) {
+  
+    btnode_t* s = NULL;
+    btnode_t* p = NULL;
+
+
+    if (node->left != NULL)
+        s = node->left;
+    else
+        s = node->right;
+
+    if (node == tree->root) {
+
+        tree->root = s;
+        p = NULL;
+
+    } else {
+
+      p = node->parent;
+
+      if (p->left == node)
+          p->left = s;
+      else
+          p->right = s;
+
+    }
+
+    if (s != NULL)
+        s->parent = p;
+
+    free(node->data);
+    free(node);
+}
+
+static void recursive_destroy_node(btnode_t* node) {
+
+  if (node == NULL)
+    return;
+
+  recursive_destroy_node(node->left);
+  recursive_destroy_node(node->right);
+
+  free(node->data);
+  free(node);
+}
+
+void binarysearchtree_add(binarysearchtree_t* tree, void* elem) {
     
     btnode_t* ins_node;
     int cmp_result;
     
     assert((void *)tree != NULL);
-    assert(data != NULL);
+    assert(elem != NULL);
     
     /* find out where we place the new data */
-    ins_node = find_node(tree, data);
+    ins_node = find_node(tree, elem);
     
     if (ins_node == NULL) {
         
         /* tree is empty, insert to root */
-        tree->root = make_node(tree, NULL, NULL, NULL, data);
+        tree->root = make_node(tree, NULL, NULL, NULL, elem);
         
     } else {
         
-        cmp_result = tree->cmp(data, ins_node->data);
+        cmp_result = tree->cmp(elem, ins_node->data);
         
         if (cmp_result < 0)
-            ins_node->left = make_node(tree, ins_node, NULL, NULL, data);
+            ins_node->left = make_node(tree, ins_node, NULL, NULL, elem);
         else if (cmp_result > 0)
-            ins_node->right = make_node(tree, ins_node, NULL, NULL, data);
+            ins_node->right = make_node(tree, ins_node, NULL, NULL, elem);
         else /* already in the tree */
             return;
         
     }
     
     ++tree->length;
+}
+
+void binarysearchtree_dispose(binarysearchtree_t* tree) {
+
+  assert((void *)tree != NULL);
+
+  recursive_destroy_node(tree->root);
 }
 
 int binarysearchtree_find(binarysearchtree_t* tree, void* search) {
@@ -119,4 +172,35 @@ void binarysearchtree_init(binarysearchtree_t* tree, size_t elem_size,
     tree->length    = 0;
     tree->cmp       = comparator;
     tree->root      = NULL;
+}
+
+int binarysearchtree_remove(binarysearchtree_t* tree, void* elem) {
+  
+    btnode_t* node = NULL;
+    btnode_t* u = NULL;
+
+    assert((void *)tree != NULL);
+    assert(elem != NULL);
+
+    node = find_node(tree, elem);
+
+    if (node == NULL || tree->cmp(elem, node->data) != NULL)
+        return 0;
+
+    if (node->left == NULL || node->right == NULL) {
+
+        remove_node(tree, node);
+
+    } else {
+
+        u = node->right;
+        while (u->left != NULL)
+          u = u->left;
+
+        memcpy(node->data, u->data, tree->elem_size);
+
+        remove_node(tree, u);
+    }
+
+    --tree->length;
 }
